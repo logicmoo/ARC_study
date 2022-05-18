4ffe4f20555aa157e28a4290394e5c8a03eec0e4
Derek Larson <larson.derek.a@gmail.com>
2022-05-18 10:08:50 -0700

    Parametrize solved tasks test run

--- tests/solved_tasks.py
+++ tests/solved_tasks.py
@@ -1,17 +1,35 @@
+import argparse
 from arc.arc import ARC
 
 # TODO Task 30 needs Align action touched up
-solved_tasks = {8, 16, 31, 36, 39, 188, 309}
+solved_tasks = {8, 16, 39, 188, 309}
+
+# Large tilings tend to take a while
 large_tiling = {17, 61, 287, 305}
 
+# Tasks from the DreamCoder work, symmetry-based
 dc_solved = {87, 106, 140, 142, 150, 152, 155, 179, 380}
-mdl_solved = {10, 53, 263, 276, 374}
 
-solved_tasks |= dc_solved
-solved_tasks |= mdl_solved
+# Tasks from the Minimum Description Length work
+# These tend to be more static in structure across cases
+mdl_solved = {10, 31, 36, 53, 263, 276, 374}
 
-_arc = ARC(idxs=solved_tasks)
-_arc.set_log({"Task": 30, "Scene": 30})
-_arc.solve_tasks(quiet=True)
-for task_idx in solved_tasks:
-    assert "Solved" in _arc[task_idx].traits
+fast_solved = solved_tasks | dc_solved | mdl_solved
+all_solved = fast_solved | large_tiling
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the fastest solved tasks (~6s)")
+    parser.add_argument(
+        "-a", "--all", help="Run all solved tasks (~20s)", action="store_true"
+    )
+    args = parser.parse_args()
+
+    tasks_to_run = fast_solved
+    if args.all:
+        tasks_to_run = all_solved
+
+    _arc = ARC(idxs=tasks_to_run)
+    _arc.set_log({"Task": 30, "Scene": 30})
+    _arc.solve_tasks(quiet=True)
+    for task_idx in tasks_to_run:
+        assert "Solved" in _arc[task_idx].traits
